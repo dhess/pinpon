f52b0ae9b418202f75f3119877d023cf5530e119
Drew Hess <src@drewhess.com>
2016-08-05 14:11:51 -0700

    Find a particular topic.

--- pinpon/Main.hs
+++ pinpon/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Lens ((^.), view)
@@ -5,14 +7,21 @@ import Control.Monad.Trans.AWS
        (Region(Oregon), Credentials(Discover), newEnv, paginate,
         runResourceT, runAWST)
 import Data.Conduit (($$), (=$=))
-import qualified Data.Conduit.Combinators as Conduit (concatMap, sinkList)
+import qualified Data.Conduit.Combinators as Conduit (concatMap, find, sinkList)
+import Data.Text (Text)
 import Network.AWS (AWS)
-import Network.AWS.SNS (listTopics, ltrsTopics)
+import Network.AWS.SNS (Topic, listTopics, ltrsTopics, tTopicARN)
+
+targetARN :: Text
+targetARN = "arn:aws:sns:us-west-2:948017695415:topic_name_test1"
+
+topicMatch :: Text -> Topic -> Bool
+topicMatch arn topic = maybe False (== arn) (view tTopicARN topic)
 
 main :: IO ()
 main =
   do env <- newEnv Oregon Discover
      result <- runResourceT . runAWST env $
-       do l <- paginate listTopics $$ Conduit.concatMap (view ltrsTopics) =$= Conduit.sinkList
+       do l <- paginate listTopics $$ Conduit.concatMap (view ltrsTopics) =$= Conduit.find (topicMatch targetARN)
           return l
      print result
