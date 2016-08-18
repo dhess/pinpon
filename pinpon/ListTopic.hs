14dfc8963a0b467ca9646ca3dd038ce90acc0e93
Drew Hess <src@drewhess.com>
2016-08-18 16:49:15 -0700

    Create a topic.

--- pinpon/ListTopic.hs
+++ pinpon/ListTopic.hs
@@ -5,12 +5,12 @@ module Main where
 import Control.Lens ((^.), view)
 import Control.Monad.Trans.AWS
        (Region(Oregon), Credentials(Discover), newEnv, paginate,
-        runResourceT, runAWST, send)
+        runResourceT, runAWST)
 import Data.Conduit (($$), (=$=))
 import qualified Data.Conduit.Combinators as Conduit (concatMap, find, sinkList)
 import Data.Text (Text)
 import Network.AWS (AWS)
-import Network.AWS.SNS (Topic, createTopic, listTopics, ltrsTopics, tTopicARN)
+import Network.AWS.SNS (Topic, listTopics, ltrsTopics, tTopicARN)
 
 targetARN :: Text
 targetARN = "arn:aws:sns:us-west-2:948017695415:topic_name_test1"
@@ -22,7 +22,6 @@ main :: IO ()
 main =
   do env <- newEnv Oregon Discover
      result <- runResourceT . runAWST env $
-       do topic <- send $ createTopic "test1"
-          l <- paginate listTopics $$ Conduit.concatMap (view ltrsTopics) =$= Conduit.sinkList
+       do l <- paginate listTopics $$ Conduit.concatMap (view ltrsTopics) =$= Conduit.find (topicMatch targetARN)
           return l
      print result
