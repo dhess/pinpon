eeaaac444b9a6d9801d6ce32b7fa345e7d2df2a0
Drew Hess <src@drewhess.com>
2016-10-05 18:03:59 -0700

    Publish a message to a given target ARN.

--- pinpon/Main.hs
+++ pinpon/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Control.Lens ((^.), view)
+import Control.Lens ((&), (?~), (^.), view)
 import Control.Monad.Trans.AWS
        (Region(Oregon), Credentials(Discover), newEnv, paginate,
         runResourceT, runAWST, send)
@@ -10,19 +10,19 @@ import Data.Conduit (($$), (=$=))
 import qualified Data.Conduit.Combinators as Conduit (concatMap, find, sinkList)
 import Data.Text (Text)
 import Network.AWS (AWS)
-import Network.AWS.SNS (Topic, createTopic, listTopics, ltrsTopics, tTopicARN)
+import Network.AWS.SNS (Topic, createTopic, ctrsTopicARN, tTopicARN)
+import Network.AWS.SNS.Publish (publish, pSubject, pTargetARN)
 
 targetARN :: Text
-targetARN = "arn:aws:sns:us-west-2:948017695415:topic_name_test1"
-
-topicMatch :: Text -> Topic -> Bool
-topicMatch arn topic = maybe False (== arn) (view tTopicARN topic)
+targetARN = "arn:aws:sns:us-west-2:948017695415:test1"
 
 main :: IO ()
 main =
   do env <- newEnv Oregon Discover
      result <- runResourceT . runAWST env $
        do topic <- send $ createTopic "test1"
-          l <- paginate listTopics $$ Conduit.concatMap (view ltrsTopics) =$= Conduit.sinkList
-          return l
+          r <- send $ publish "Yo"
+                        & pSubject ?~ "Hi from Amazon SNS"
+                        & pTargetARN ?~ targetARN
+          return r
      print result
