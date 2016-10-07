5867edc62394dad7c9e1f6563a6fd86ea777bd8b
Drew Hess <src@drewhess.com>
2016-10-06 20:30:29 -0700

    Add a dummy Notification type.

--- src/Network/PinPon/Server/API.hs
+++ src/Network/PinPon/Server/API.hs
@@ -15,6 +15,7 @@ the REST service methods and document types.
 -}
 
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -24,6 +25,7 @@ module Network.PinPon.Server.API
          ( -- * Types
            PinPonAPI
          , Config(..)
+         , Notification(..)
 
            -- * Servant / WAI functions
          , app
@@ -34,19 +36,53 @@ module Network.PinPon.Server.API
 import Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
 import Control.Monad.Trans.Except (ExceptT)
 import Control.Monad.IO.Class (MonadIO)
+import Data.Aeson.Types
+       (FromJSON(..), ToJSON(..), Options(..), camelTo2, defaultOptions,
+        genericParseJSON, genericToEncoding)
 import qualified Data.Map.Strict as Map (Map, lookup)
 import Data.Text (Text)
+import GHC.Generics
+import Lucid
+       (ToHtml(..), HtmlT, doctypehtml_, head_, title_, body_)
 import Network.Wai (Application)
 import Servant
-       ((:>), (:~>)(..), Capture, JSON, Post, Proxy(..), ServerT, Server,
-        ServantErr(..), enter, err404, serve, throwError)
+       ((:>), (:~>)(..), Capture, JSON, Post, Proxy(..), ReqBody, ServerT,
+        Server, ServantErr(..), enter, err404, serve, throwError)
 import Servant.HTML.Lucid (HTML)
+
+localOptions :: Options
+localOptions =
+  defaultOptions {fieldLabelModifier = drop 1
+                 ,constructorTagModifier = camelTo2 '_'}
 
 data Config =
   Config {_keyToTopic :: Map.Map Text Text}
 
+data Notification =
+  Notification {_body :: Text}
+  deriving (Generic)
+
+instance ToJSON Notification where
+  toEncoding = genericToEncoding localOptions
+instance FromJSON Notification where
+  parseJSON = genericParseJSON localOptions
+
+notificationDocument :: Monad m => HtmlT m a -> HtmlT m a
+notificationDocument = wrapBody "PinPon Notification"
+
+instance ToHtml Notification where
+  toHtml (Notification body) = notificationDocument $ toHtml body
+  toHtmlRaw = toHtml
+
+wrapBody :: Monad m => HtmlT m () -> HtmlT m a -> HtmlT m a
+wrapBody title body =
+  doctypehtml_ $
+    do head_ $
+         title_ title
+       body_ body
+
 type PinPonAPI =
-  "notify" :> Capture "key" Text :> Post '[JSON, HTML] Text
+  "notify" :> Capture "key" Text :> ReqBody '[JSON] Notification :> Post '[JSON, HTML] Notification
 
 type AppM c m = ReaderT Config (ExceptT ServantErr m)
 
@@ -54,12 +90,12 @@ serverT :: (MonadIO m) => ServerT PinPonAPI (AppM c m)
 serverT =
   notify
   where
-    notify :: (MonadIO m) => Text -> AppM c m Text
-    notify key =
+    notify :: (MonadIO m) => Text -> Notification -> AppM c m Notification
+    notify key n =
       do config <- ask
          case Map.lookup key (_keyToTopic config) of
            Nothing -> throwError $ err404 { errBody = "key not found" }
-           Just arn -> return arn
+           Just _ -> return n
 
 pinPonAPI :: Proxy PinPonAPI
 pinPonAPI = Proxy
