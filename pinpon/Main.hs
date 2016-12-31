3373ad464823a6f2fb72d3907af6c441e96cc7c5
Drew Hess <src@drewhess.com>
2016-12-31 13:48:36 -0800

    Add ability to specify region to pinpon server.
    Also, default is now us-east-1 per standard AWS practice.

--- pinpon/Main.hs
+++ pinpon/Main.hs
@@ -4,11 +4,13 @@ module Main where
 
 import Control.Lens ((^.))
 import Control.Monad.Trans.AWS
-       (Region(Oregon), Credentials(Discover), newEnv, runResourceT,
-        runAWST, send)
+       (Region(..), Credentials(Discover), newEnv, runResourceT, runAWST,
+        send)
+
 import Data.Maybe (fromJust)
 import Data.Text (Text)
 import Network (PortID(..), listenOn)
+import Network.AWS.Data (fromText)
 import Network.AWS.SNS (createTopic, ctrsTopicARN)
 import Network.PinPon.Config (Config(..))
 import Network.PinPon.SwaggerAPI (app)
@@ -16,12 +18,21 @@ import Network.Wai.Handler.Warp (defaultSettings, runSettingsSocket, setHost, se
 import Options.Applicative
 import Options.Applicative.Text (text)
 
-data Options = Options {_port :: !Int
-                       ,_topicName :: !Text}
+data Options = Options
+  { _region :: !Region
+  , _port :: !Int
+  , _topicName :: !Text
+  }
 
-defaultConfig :: Text -> IO Config
-defaultConfig topicName =
-  do env <- newEnv Oregon Discover
+parseRegion :: Text -> ReadM Region
+parseRegion r =
+  case fromText r of
+    Left _ -> readerError $ "Invalid AWS region: " ++ show r
+    Right region -> return region
+
+defaultConfig :: Region -> Text -> IO Config
+defaultConfig region topicName =
+  do env <- newEnv region Discover
      topic <- runResourceT . runAWST env $
         send $ createTopic topicName
      return Config {_env = env
@@ -30,16 +41,22 @@ defaultConfig topicName =
 options :: Parser Options
 options =
   Options <$>
+  option (text >>= parseRegion)
+         (long "region" <>
+          short 'r' <>
+          metavar "REGION" <>
+          value NorthVirginia <>
+          help "Specify the AWS region (default is us-east-1)") <*>
   option auto (long "port" <>
                short 'p' <>
                metavar "INT" <>
                value 8000 <>
                help "Listen on port") <*>
-  argument text (metavar "TOPIC_NAME")
+  argument text (metavar "topic-name")
 
 run :: Options -> IO ()
-run (Options port topicName) =
-  do config <- defaultConfig $ topicName
+run (Options region port topicName) =
+  do config <- defaultConfig region $ topicName
      sock <- listenOn (PortNumber (fromIntegral port))
      runSettingsSocket (setPort port $ setHost "*" defaultSettings) sock (app config)
 
