d51e26de1d386cff5d9a74307b3f98f54978aa96
Drew Hess <src@drewhess.com>
2016-12-31 15:56:51 -0800

    Add ability to specify credentials file and profile for server.

--- pinpon/Main.hs
+++ pinpon/Main.hs
@@ -1,15 +1,15 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Control.Lens ((^.))
 import Control.Monad.Trans.AWS
-       (Region(..), Credentials(Discover), newEnv, runResourceT, runAWST,
-        send)
-
+       (Region(..), Credentials(..), newEnv, runResourceT, runAWST, send)
 import Data.Maybe (fromJust)
 import Data.Text (Text)
 import Network (PortID(..), listenOn)
+import Network.AWS.Auth (credProfile, credFile)
 import Network.AWS.Data (fromText)
 import Network.AWS.SNS (createTopic, ctrsTopicARN)
 import Network.PinPon.Config (Config(..))
@@ -20,6 +20,8 @@ import Options.Applicative.Text (text)
 
 data Options = Options
   { _region :: !Region
+  , _credentialsFile :: Maybe FilePath
+  , _credentialsProfile :: !Text
   , _port :: !Int
   , _topicName :: !Text
   }
@@ -30,9 +32,16 @@ parseRegion r =
     Left _ -> readerError $ "Invalid AWS region: " ++ show r
     Right region -> return region
 
-defaultConfig :: Region -> Text -> IO Config
-defaultConfig region topicName =
-  do env <- newEnv region Discover
+makeCredentials :: Maybe FilePath -> Text -> IO Credentials
+makeCredentials Nothing profile =
+  do defaultPath <- credFile
+     return $ FromFile profile defaultPath
+makeCredentials (Just path) profile =
+  return $ FromFile profile path
+
+defaultConfig :: Region -> Credentials -> Text -> IO Config
+defaultConfig region credentials topicName =
+  do env <- newEnv region credentials
      topic <- runResourceT . runAWST env $
         send $ createTopic topicName
      return Config {_env = env
@@ -47,6 +56,15 @@ options =
           metavar "REGION" <>
           value NorthVirginia <>
           help "Specify the AWS region (default is us-east-1)") <*>
+  optional (strOption (long "credentials-file" <>
+                       short 'f' <>
+                       metavar "PATH" <>
+                       help "Path to AWS credentials file")) <*>
+  option text (long "profile" <>
+               short 'P' <>
+               metavar "PROFILE_NAME" <>
+               value credProfile <>
+               help "Credentials profile name") <*>
   option auto (long "port" <>
                short 'p' <>
                metavar "INT" <>
@@ -55,10 +73,12 @@ options =
   argument text (metavar "topic-name")
 
 run :: Options -> IO ()
-run (Options region port topicName) =
-  do config <- defaultConfig region $ topicName
+run (Options region maybeFile profile port topicName) =
+  do credentials <- makeCredentials maybeFile profile
+     config <- defaultConfig region credentials topicName
      sock <- listenOn (PortNumber (fromIntegral port))
      runSettingsSocket (setPort port $ setHost "*" defaultSettings) sock (app config)
+
 
 main :: IO ()
 main = execParser opts >>= run
