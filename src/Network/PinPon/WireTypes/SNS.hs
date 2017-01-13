9e49a53c4d6f11af2220bcacd9c25b6a34cf9bea
Drew Hess <src@drewhess.com>
2017-01-13 00:45:30 -1000

    Proper APNS encoding.
    Fixes the issue mentioned in the previous commit (APNS JSON is now
    encoded as a string, rather than as a nested object).
    
    Note that the Apple APNS docs state that JSON \U notation is not
    supported, so I need to test UTF-8 character encoding as I'm pretty
    certain that Aeson will generate \U notation per the standard.

--- src/Network/PinPon/WireTypes/SNS.hs
+++ src/Network/PinPon/WireTypes/SNS.hs
@@ -17,6 +17,7 @@ import Data.Monoid ((<>))
 import Data.Text (Text)
 import GHC.Generics
 
+import Network.PinPon.Util (encodeText)
 import qualified Network.PinPon.WireTypes.APNS as APNS (Payload(..))
 
 -- $setup
@@ -24,7 +25,7 @@ import qualified Network.PinPon.WireTypes.APNS as APNS (Payload(..))
 
 -- | A multi-platform SNS message.
 data Message = Message
-  { _defaultMsg         :: !Text               -- ^ The default message (required by SNS)
+  { _defaultMsg         :: !Text              -- ^ The default message (required by SNS)
   , _apnsPayload        :: Maybe APNS.Payload -- ^ Optional production APNS payload
   , _apnsSandboxPayload :: Maybe APNS.Payload -- ^ Optional sandbox APNS payload
   } deriving (Show, Generic)
@@ -36,18 +37,18 @@ instance ToJSON Message where
     where
       apns :: Maybe APNS.Payload -> [Pair]
       apns Nothing = mempty
-      apns v = ["APNS" .= v]
+      apns v = ["APNS" .= encodeText v]
       apnsSandbox :: Maybe APNS.Payload -> [Pair]
       apnsSandbox Nothing = mempty
-      apnsSandbox v = ["APNS_SANDBOX" .= v]
+      apnsSandbox v = ["APNS_SANDBOX" .= encodeText v]
   toEncoding (Message d p s) = pairs $! "default" .= d <> apns p <> apnsSandbox s
     where
       apns :: Maybe APNS.Payload -> Series
       apns Nothing = mempty
-      apns v = "APNS" .= v
+      apns v = "APNS" .= encodeText v
       apnsSandbox :: Maybe APNS.Payload -> Series
       apnsSandbox Nothing = mempty
-      apnsSandbox v = "APNS_SANDBOX" .= v
+      apnsSandbox v = "APNS_SANDBOX" .= encodeText v
 
 -- $
 -- >>> import Network.PinPon.WireTypes.APNS (Alert(..), Aps(..), Payload(..))
@@ -56,19 +57,19 @@ instance ToJSON Message where
 -- >>> let payload1 = Payload aps1
 -- >>> let msg1 = Message "This is the default message" (Just payload1) Nothing
 -- >>> toJSON msg1
--- Object (fromList [("default",String "This is the default message"),("APNS",Object (fromList [("aps",Object (fromList [("sound",String "production default"),("alert",Object (fromList [("body",String "This is a production alert body"),("title",String "This is a production alert title")]))]))]))])
+-- Object (fromList [("default",String "This is the default message"),("APNS",String "{\"aps\":{\"alert\":{\"title\":\"This is a production alert title\",\"body\":\"This is a production alert body\"},\"sound\":\"production default\"}}")])
 -- >>> toEncoding msg1
--- "{\"default\":\"This is the default message\",\"APNS\":{\"aps\":{\"alert\":{\"title\":\"This is a production alert title\",\"body\":\"This is a production alert body\"},\"sound\":\"production default\"}}}"
+-- "{\"default\":\"This is the default message\",\"APNS\":\"{\\\"aps\\\":{\\\"alert\\\":{\\\"title\\\":\\\"This is a production alert title\\\",\\\"body\\\":\\\"This is a production alert body\\\"},\\\"sound\\\":\\\"production default\\\"}}\"}"
 -- >>> let alert2 = Alert "This is a sandbox alert title" "This is a sandbox alert body"
 -- >>> let aps2 = Aps alert2 "sandbox default"
 -- >>> let payload2 = Payload aps2
 -- >>> let msg2 = Message "This is the default message" Nothing (Just payload2)
 -- >>> toJSON msg2
--- Object (fromList [("default",String "This is the default message"),("APNS_SANDBOX",Object (fromList [("aps",Object (fromList [("sound",String "sandbox default"),("alert",Object (fromList [("body",String "This is a sandbox alert body"),("title",String "This is a sandbox alert title")]))]))]))])
+-- Object (fromList [("default",String "This is the default message"),("APNS_SANDBOX",String "{\"aps\":{\"alert\":{\"title\":\"This is a sandbox alert title\",\"body\":\"This is a sandbox alert body\"},\"sound\":\"sandbox default\"}}")])
 -- >>> toEncoding msg2
--- "{\"default\":\"This is the default message\",\"APNS_SANDBOX\":{\"aps\":{\"alert\":{\"title\":\"This is a sandbox alert title\",\"body\":\"This is a sandbox alert body\"},\"sound\":\"sandbox default\"}}}"
+-- "{\"default\":\"This is the default message\",\"APNS_SANDBOX\":\"{\\\"aps\\\":{\\\"alert\\\":{\\\"title\\\":\\\"This is a sandbox alert title\\\",\\\"body\\\":\\\"This is a sandbox alert body\\\"},\\\"sound\\\":\\\"sandbox default\\\"}}\"}"
 -- >>> let msg3 = Message "This is the default message" (Just payload1) (Just payload2)
 -- >>> toJSON msg3
--- Object (fromList [("default",String "This is the default message"),("APNS_SANDBOX",Object (fromList [("aps",Object (fromList [("sound",String "sandbox default"),("alert",Object (fromList [("body",String "This is a sandbox alert body"),("title",String "This is a sandbox alert title")]))]))])),("APNS",Object (fromList [("aps",Object (fromList [("sound",String "production default"),("alert",Object (fromList [("body",String "This is a production alert body"),("title",String "This is a production alert title")]))]))]))])
+-- Object (fromList [("default",String "This is the default message"),("APNS_SANDBOX",String "{\"aps\":{\"alert\":{\"title\":\"This is a sandbox alert title\",\"body\":\"This is a sandbox alert body\"},\"sound\":\"sandbox default\"}}"),("APNS",String "{\"aps\":{\"alert\":{\"title\":\"This is a production alert title\",\"body\":\"This is a production alert body\"},\"sound\":\"production default\"}}")])
 -- >>> toEncoding msg3
--- "{\"default\":\"This is the default message\",\"APNS\":{\"aps\":{\"alert\":{\"title\":\"This is a production alert title\",\"body\":\"This is a production alert body\"},\"sound\":\"production default\"}},\"APNS_SANDBOX\":{\"aps\":{\"alert\":{\"title\":\"This is a sandbox alert title\",\"body\":\"This is a sandbox alert body\"},\"sound\":\"sandbox default\"}}}"
+-- "{\"default\":\"This is the default message\",\"APNS\":\"{\\\"aps\\\":{\\\"alert\\\":{\\\"title\\\":\\\"This is a production alert title\\\",\\\"body\\\":\\\"This is a production alert body\\\"},\\\"sound\\\":\\\"production default\\\"}}\",\"APNS_SANDBOX\":\"{\\\"aps\\\":{\\\"alert\\\":{\\\"title\\\":\\\"This is a sandbox alert title\\\",\\\"body\\\":\\\"This is a sandbox alert body\\\"},\\\"sound\\\":\\\"sandbox default\\\"}}\"}"
