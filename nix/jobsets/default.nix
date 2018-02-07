80e87601feba671f1c501f0cc31e1e8b68cd6433
Drew Hess <dhess-src@quixoftic.com>
2018-02-06 18:11:27 -0800

    Add new job types to default.nix.

--- nix/jobsets/default.nix
+++ nix/jobsets/default.nix
@@ -15,7 +15,13 @@ let
     emailresponsible = false;
   };
 
+  ## nixpkgs-stackage wants a <nixpkgs> path so that it can import
+  ## Haskell functions that nixpkgs doesn't explicitly export.
   nixpkgs-src = builtins.fromJSON (builtins.readFile ../nixpkgs-src.json);
+  nixpkgs-spec = {
+    url = "https://github.com/${nixpkgs-src.owner}/${nixpkgs-src.repo}.git";
+    rev = "${nixpkgs-src.rev}";
+  };
 
   pkgs = import nixpkgs {};
 
@@ -31,22 +37,55 @@ let
     nixexprinput = "pinpon";
     description = "pinpon";
     inputs = {
+
+      ## Note: the nixpkgs input here is for nixpkgs-stackage's
+      ## <nixpkgs>. It is not used by pinpon.
+      nixpkgs = mkFetchGithub "${nixpkgs-spec.url} ${nixpkgs-spec.rev}";
+
       pinpon = mkFetchGithub "${pinponUri} master";
+
     };
   };
 
-  mkChannelAlt = pinponBranch: nixpkgsRev: {
-    checkinterval = 60;
-    schedulingshares = 100;
+  mkChannelAlt = pinponBranch: nixpkgsRev: nixpkgsStackageRev: {
     inputs = {
+
+      ## Note: the nixpkgs input here is for nixpkgs-stackage's
+      ## <nixpkgs>. It is not used by pinpon.
+      nixpkgs = mkFetchGithub "${nixpkgs-spec.url} ${nixpkgs-spec.rev}";
+
       nixpkgs_override = mkFetchGithub "https://github.com/NixOS/nixpkgs-channels.git ${nixpkgsRev}";
+      nixpkgs_stackage_override = mkFetchGithub "https://github.com/typeable/nixpkgs-stackage.git ${nixpkgsStackageRev}";
       pinpon = mkFetchGithub "${pinponUri} ${pinponBranch}";
+
+    };
+  };
+
+
+  ## "next" builds; these are expected to fail from time to time and
+  ## don't run as often. They also build from nixpkgs and not
+  ## nixpkgs-channels as we always want to be testing the latest,
+  ## greatest GHC pre-releases. (We don't bother overriding
+  ## nixpkgs-stackage because it's not used in these evaluations.)
+
+  mkNext = pinponBranch: nixpkgsRev: {
+    nixexprpath = "nix/jobsets/next.nix";
+    checkinterval = 60 * 60 * 24;
+    inputs = {
+
+      ## Note: does not depend on nixpkgs because we don't use
+      ## nixpkgs-stackage for these builds.
+
+      nixpkgs_override = mkFetchGithub "https://github.com/NixOS/nixpkgs.git ${nixpkgsRev}";
+      pinpon = mkFetchGithub "${pinponUri} ${pinponBranch}";
+
     };
   };
 
   mainJobsets = with pkgs.lib; mapAttrs (name: settings: defaultSettings // settings) (rec {
     master = {};
-    nixpkgs-unstable = mkChannelAlt "master" "nixpkgs-unstable";
+    nixpkgs-unstable = mkChannelAlt "master" "nixpkgs-unstable" "master";
+    next-ghc = mkNext "master" "master";
   });
 
   jobsetsAttrs = mainJobsets;
